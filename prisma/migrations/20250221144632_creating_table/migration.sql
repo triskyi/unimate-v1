c3d87e9cfca11deb5c0be20f37fcdd24f460a038
triskyi <tresorbi29@gmail.com>
2025-02-21 17:47:08 +0300

    modified:   package-lock.json 	modified:   package.json 	new file:   prisma/migrations/20250221144632_creating_table/migration.sql 	modified:   prisma/migrations/migration_lock.toml 	modified:   prisma/schema.prisma

---
+++ prisma/migrations/20250221144632_creating_table/migration.sql
@@ -0,0 +1,32 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Admin` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `PaymentTransaction` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Post` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "PaymentTransaction" DROP CONSTRAINT "PaymentTransaction_userId_fkey";
+
+-- DropTable
+DROP TABLE "Admin";
+
+-- DropTable
+DROP TABLE "PaymentTransaction";
+
+-- DropTable
+DROP TABLE "Post";
+
+-- DropTable
+DROP TABLE "User";
+
+-- CreateTable
+CREATE TABLE "playing_with_neon" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "value" REAL,
+
+    CONSTRAINT "playing_with_neon_pkey" PRIMARY KEY ("id")
+);
